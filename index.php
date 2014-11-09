2941fbb30eb36dec7c41518781431ac1132049b7
Ollie Murphy <olivermurphy@Olivers-MacBook-Pro.local>
2014-11-09 21:22:46 +0000

    Create

---
+++ index.php
@@ -0,0 +1,83 @@
+<html lang="en">
+<head>
+	<!--Bootstrap Css-->
+	<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!--Main Css-->
+	<link href="css/main_stylesheet.css" rel="stylesheet">
+</head>
+
+ <body>
+ 
+	 <div class="main_image_container">
+	 	 <div class="header_home">
+		 	 <ul>
+			 	 <li>Register</li>
+			 	 <li><img height="100px" width="100px" src="images/hunter-logo-2.png"></li>
+			 	 <li>How it works</li>
+		 	 </ul>
+	 	 </div>
+		 <h1>You will get that Job</h1>
+		 <p>You're awesome</p>
+	 </div>
+	 
+	 <div class="container-fluid welcome">
+		 <div class="row">
+			 <div class="col-md-10 col-md-offset-1 text-center">
+				<div class="welcome_title">
+					 Welcome
+				</div>
+					<p class="margin-top-15">"Find out what you like doing best and get someone to pay you for doing it." - Katherine Whitehorn</p>
+			 </div>
+		 </div>
+	 </div>
+	 
+	 <div class="logins_container">
+		 <div class="center_login">
+			 <div class="login_box">
+				 <div class="login_header">
+					<span class="glyphicon glyphicon-user"></span> Employer
+				 </div>
+				 <div class="login_body">
+					 <input type="text" placeholder="Username">
+					 <input type="password" placeholder="Password">
+					 <input type="submit" value="Log In">
+					 <p class="margin-bottom-none margin-top-15">Forgotten your password?</p>
+					 <p class="no-margin">Don't have an account? <input id="sign_up_button" type="submit" value="Sign Up"></p>
+				 </div>
+			 </div>
+			 <div class="login_box">
+				 <div class="login_header">
+					<span class="glyphicon glyphicon-user"></span> Employee
+				 </div>
+				 <div class="login_body">
+					 <input type="text" placeholder="Username">
+					 <input type="password" placeholder="Password">
+					 <input type="submit" value="Log In">
+					 <p class="margin-bottom-none margin-top-15">Forgotten your password?</p>
+					 <p class="no-margin">Don't have an account? <input id="sign_up_button" type="submit" value="Sign Up"></p>
+				 </div>				 
+			 </div>
+		 </div>
+	 </div>
+	 
+	 <div class="clear_float margin-bottom-30"></div>
+
+
+
+	 <div class="bottom_image_container">
+		 <h1>Let the jobs find you at HUNTER</h1>
+		 <p>We're awesome</p>
+	 </div>
+
+		 
+ </body>
+<!--
+
+<footer>	
+</footer>
+-->
+</html>
+
+<!--Bootstrap Js-->
+<script src="bootstrap.min.js"></script>
