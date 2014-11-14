ff9762169c0cf42219db430e95feb04683a16420
Ollie Murphy <olivermurphy@Olivers-MacBook-Pro.local>
2014-11-14 10:00:29 +0000

    update

---
+++ landingpage.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--Bootstrap Css-->
+	<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!--Main Css-->
+	<link href="css/main_stylesheet.css" rel="stylesheet">
+	<link href="css/standard_stylesheet.css" rel="stylesheet">
+</head>
+
+ <body>
+ 
+ <div class="landing_background">
+	 <h1 class="m-none p-t-150 p-l-40">Stop Looking For Jobs</h1>
+	 <h3 class="p-l-40">Let them find you at Career Huntr</h3>
+	 <h4 class="p-l-40">Coming in the next few months...</h4>
+	 
+ </div>
+		 
+ </body>
+<!--
+
+<footer>	
+</footer>
+-->
+</html>
+
+<!--Bootstrap Js-->
+<script src="bootstrap.min.js"></script>
